f90def236ea74770fa6816db24c68194247e432f
pdol <111592903+pdol9@users.noreply.github.com>
2024-10-04 14:35:49 +0200

    added and connected led matrix and 5V power supply added test led sequence

--- C-cpp-written-projects/ESP8266-web-server/task/task.ino
+++ C-cpp-written-projects/ESP8266-web-server/task/task.ino
@@ -1,10 +1,25 @@
+/*
+  Red and green LED connected to GPIO D0 and D1
+  Added:
+  - 5V Power Supply Module, 
+  - LED Matrix WS2812b 8x8 connected to D2
+  - 330 Ohm resistor between D2 and matrix Data-in pin
+*/
+
 #include "LittleFS.h"
 #include <ESPAsyncWebServer.h>
+#include <Adafruit_NeoPixel.h>
+
+#define PIN_WS2812B  D2
+#define NUM_PIXELS   64
+#define DELAY_INTERVAL 250
 
 const char *ssid = "*****";
 const char *password = "*****";
 
 AsyncWebServer server(80);
+
+Adafruit_NeoPixel WS2812B(NUM_PIXELS, PIN_WS2812B, NEO_GRB + NEO_KHZ800);
 
 // store the current output state
 String redLEDState = "off";
@@ -71,8 +86,32 @@ void setup() {
     }
     request->send(200, "text/plain", greenLEDState);
   });
-
   server.begin();
+  WS2812B.begin();
 }
 
-void loop() {}
+void loop() {
+
+  WS2812B.clear();
+
+  // turn pixels to green one by one with delay between each pixel
+  for (int pixel = 0; pixel < NUM_PIXELS; ++pixel) {
+    WS2812B.setPixelColor(pixel, WS2812B.Color(0, 255, 0));
+    WS2812B.show();
+    delay(DELAY_INTERVAL);
+  }
+  delay(1000);
+  // turn on all pixels to pink at the same time
+  for (int pixel = 0; pixel < NUM_PIXELS; ++pixel) {
+    WS2812B.setPixelColor(pixel, WS2812B.Color(255, 0, 255));
+  }
+  WS2812B.show();
+  delay(1000);
+  // count down by turning pixels to blue
+  for (int pixel = NUM_PIXELS - 1; pixel >= 0; --pixel) {
+    WS2812B.setPixelColor(pixel, WS2812B.Color(0, 0, 255));
+    WS2812B.show();
+    delay(DELAY_INTERVAL);
+  }
+  delay(2000);
+}
