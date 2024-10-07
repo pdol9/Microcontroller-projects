1b3d6ac84a370d2e3ee940e462a33d32d13b1272
pdol <111592903+pdol9@users.noreply.github.com>
2024-10-07 11:17:21 +0200

    add new color sequences and include them into server adjust UI (dropmenu) to use new features

--- C-cpp-written-projects/ESP8266-web-server/task/seq.ino
+++ C-cpp-written-projects/ESP8266-web-server/task/seq.ino
@@ -1,7 +1,6 @@
 // color sequences
 
-// avalanche
-void	seq_1() {
+void	avalanche() {
 
 	WS2812B.clear();
 	for (uint16_t pixel = 0; pixel < NUM_PIXELS; ++pixel) {
@@ -43,12 +42,10 @@ void	seq_1() {
 		WS2812B.setPixelColor(pixel, WS2812B.Color(255, 255, 255));
 		WS2812B.show();
 	}
-
 	delay(1000);
 }
 
-// rainbow avalanche
-void  seq_2() {
+void  rainbow_avalanche() {
 	WS2812B.clear();
 	for (uint16_t i = 0; i < 8; ++i) {
 		for (uint16_t pixel = 0; pixel < NUM_PIXELS; ++pixel) {
@@ -62,8 +59,7 @@ void  seq_2() {
 	delay(1000);
 }
 
-// stacking color
-void  seq_3() {
+void  stacking_colors() {
 	WS2812B.clear();
 
 	for (uint16_t i = 0; i < 8; ++i) {
@@ -81,8 +77,7 @@ void  seq_3() {
 	}
 }
 
-// stacking rainbow
-void  seq_4() {
+void  stacking_rainbow() {
 	WS2812B.clear();
 	for (uint16_t stacked = 0; stacked < NUM_PIXELS; ++stacked) {
 		for (uint16_t i = 0; i < 8; ++i, ++stacked) {
@@ -100,7 +95,7 @@ void  seq_4() {
 	}
 }
 
-void	rainbow() {
+void	draw_rainbow() {
 	WS2812B.clear();
 	for (uint16_t pixel = 0; pixel < 8; ++pixel) {
 		for (uint16_t i = 0; i < 8; ++i) {
@@ -110,4 +105,67 @@ void	rainbow() {
 		delay(100);
 	}
 	delay(2000);
+}
+
+/*  using existing libraries  */
+
+void slow_drift() {
+
+	static uint16_t i = 0;
+	for (int j=0; j < WS2812B.numPixels(); ++j) {
+		WS2812B.setPixelColor(j, Wheel((i + j) & 255));
+	}
+	WS2812B.show();
+	i++;
+	delay(DELAY_INTERVAL);
+}
+
+void color_palette() {
+
+	static uint16_t i = 0;
+	WS2812B.clear();
+	for (int j = 0; j < WS2812B.numPixels(); ++j) {
+		WS2812B.setPixelColor(j, Wheel((i + j * 10) & 255));
+	}
+	WS2812B.show();
+	i++;
+	delay(DELAY_INTERVAL);
+}
+
+void switching_rgb() {
+
+	static uint8_t colorIndex = 0;
+	uint32_t colors[] = {WS2812B.Color(255, 0, 0), WS2812B.Color(0, 255, 0),
+		WS2812B.Color(0, 0, 255), WS2812B.Color(255, 255, 0)};
+	WS2812B.clear();
+	for (int j = 0; j < WS2812B.numPixels(); ++j) {
+		WS2812B.setPixelColor(j, colors[colorIndex]);
+	}
+	WS2812B.show();
+	colorIndex = (colorIndex + 1) % 4;
+	delay(DELAY_INTERVAL * 10);
+}
+
+void tracer() {
+
+	static int pos = 0;
+	WS2812B.clear();
+	WS2812B.setPixelColor(pos, WS2812B.Color(255, 255, 255));
+	WS2812B.show();
+	pos = (pos + 1) % WS2812B.numPixels();
+	delay(DELAY_INTERVAL);
+}
+
+// Helper function to generate rainbow colors
+uint32_t Wheel(byte WheelPos) {
+	WheelPos = 255 - WheelPos;
+	if (WheelPos < 85) {
+		return WS2812B.Color(255 - WheelPos * 3, 0, WheelPos * 3);
+	}
+	if (WheelPos < 170) {
+		WheelPos -= 85;
+		return WS2812B.Color(0, WheelPos * 3, 255 - WheelPos * 3);
+	}
+	WheelPos -= 170;
+	return WS2812B.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
 }
--- C-cpp-written-projects/ESP8266-web-server/task/task.ino
+++ C-cpp-written-projects/ESP8266-web-server/task/task.ino
@@ -1,10 +1,10 @@
 /*
-  Red and green LED connected to GPIO D0 and D1
-  Added:
-  - 5V Power Supply Module, 
-  - LED Matrix WS2812b 8x8 connected to D2
-  - 330 Ohm resistor between D2 and matrix Data-in pin
-*/
+   Red and green LED connected to GPIO D0 and D1
+Added:
+- 5V Power Supply Module, 
+- LED Matrix WS2812b 8x8 connected to D2
+- 330 Ohm resistor between D2 and matrix Data-in pin
+ */
 
 #include "LittleFS.h"
 #include <ESPAsyncWebServer.h>
@@ -24,6 +24,7 @@ Adafruit_NeoPixel WS2812B(NUM_PIXELS, PIN_WS2812B, NEO_GRB + NEO_KHZ800);
 // store the current output state
 String redLEDState = "off";
 String greenLEDState = "off";
+String currentPattern = "rainbow";
 
 // Assign output variables to GPIO pins
 const uint16_t RED_LED = D0;
@@ -55,47 +56,93 @@ void setup() {
 	Serial.print(". IP address: ");
 	Serial.println(WiFi.localIP());
 
-  // Serve the main HTML page
-  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request) {
-    request->send(LittleFS, "/index.html", "text/html");
-  });
+	// Serve the main HTML page
+	server.on("/", HTTP_GET, [](AsyncWebServerRequest *request) {
+			request->send(LittleFS, "/index.html", "text/html");
+			});
 
-  // Serve the CSS file
-  server.on("/style.css", HTTP_GET, [](AsyncWebServerRequest *request) {
-    request->send(LittleFS, "/style.css", "text/css");
-  });
+	// Serve the CSS file
+	server.on("/style.css", HTTP_GET, [](AsyncWebServerRequest *request) {
+			request->send(LittleFS, "/style.css", "text/css");
+			});
 
-  // Endpoint to toggle Red LED
-  server.on("/toggleRedLED", HTTP_GET, [](AsyncWebServerRequest *request) {
-    if (redLEDState == "off") {
-      digitalWrite(RED_LED, HIGH);
-      redLEDState = "on";
-    } else {
-      digitalWrite(RED_LED, LOW);
-      redLEDState = "off";
-    }
-    request->send(200, "text/plain", redLEDState);
-  });
+	// Endpoint to toggle Red LED
+	server.on("/toggleRedLED", HTTP_GET, [](AsyncWebServerRequest *request) {
+			if (redLEDState == "off") {
+			digitalWrite(RED_LED, HIGH);
+			redLEDState = "on";
+			} else {
+			digitalWrite(RED_LED, LOW);
+			redLEDState = "off";
+			}
+			request->send(200, "text/plain", redLEDState);
+			});
 
-  // Endpoint to toggle Green LED
-  server.on("/toggleGreenLED", HTTP_GET, [](AsyncWebServerRequest *request) {
-    if (greenLEDState == "off") {
-      digitalWrite(GREEN_LED, HIGH);
-      greenLEDState = "on";
-    } else {
-      digitalWrite(GREEN_LED, LOW);
-      greenLEDState = "off";
-    }
-    request->send(200, "text/plain", greenLEDState);
-  });
-  server.begin();
-  WS2812B.begin();
+	// Endpoint to toggle Green LED
+	server.on("/toggleGreenLED", HTTP_GET, [](AsyncWebServerRequest *request) {
+			if (greenLEDState == "off") {
+			digitalWrite(GREEN_LED, HIGH);
+			greenLEDState = "on";
+			} else {
+			digitalWrite(GREEN_LED, LOW);
+			greenLEDState = "off";
+			}
+			request->send(200, "text/plain", greenLEDState);
+			});
+
+	// Endpoint to Get Current Pattern
+	server.on("/getCurrentPattern", HTTP_GET, [](AsyncWebServerRequest *request) {
+			request->send(200, "text/plain", currentPattern);
+			});
+
+	server.on("/setPattern", HTTP_GET, [](AsyncWebServerRequest *request) {
+			if (request->hasParam("pattern")) {
+			String pattern = request->getParam("pattern")->value();
+			if (pattern == "avalanche" || pattern == "rainbow_avalanche" ||
+					pattern == "stacking_colors" || pattern == "stacking_rainbow" ||
+					pattern == "draw_rainbow" || pattern == "slow_drift" ||
+					pattern == "color_palette" || pattern == "switching_rgb" ||
+					pattern == "tracer") {
+			currentPattern = pattern;
+			Serial.println("Pattern set to: " + currentPattern);
+			request->send(200, "text/plain", "Pattern set to " + currentPattern);
+			} else {
+			request->send(400, "text/plain", "Invalid pattern");
+			}
+			} else {
+			request->send(400, "text/plain", "Bad Request");
+			}
+			});
+	server.begin();
+	WS2812B.begin();
 }
 
 void loop() {
-	// rainbow();
-	// seq_1();
-	// seq_2();
-	// seq_3();
-	seq_4();
+	if (currentPattern == "avalanche") {
+		avalanche();
+	}
+	else if (currentPattern == "rainbow_avalanche") {
+		rainbow_avalanche();
+	}
+	else if (currentPattern == "stacking_colors") {
+		stacking_colors();
+	}
+	else if (currentPattern == "stacking_rainbow") {
+		stacking_rainbow();
+	}
+	else if (currentPattern == "draw_rainbow") {
+		draw_rainbow();
+	}
+	else if (currentPattern == "slow_drift") {
+		slow_drift();
+	}
+	else if (currentPattern == "color_palette") {
+		color_palette();
+	}
+	else if (currentPattern == "switching_rgb") {
+		switching_rgb();
+	}
+	else if (currentPattern == "tracer") {
+		tracer();
+	}
 }
